a43e8272a6b726a4aaeef790b436f997aceb2773
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-05-15 10:31:15 +0700

    cretae new node

--- doubleLinkList.cpp
+++ doubleLinkList.cpp
@@ -20,6 +20,9 @@ class DoubleLinkedList{
             int nim;
             string nm; 
             cout << "\nEnter the roll number of the student";
+            cin >> nim;
         }
+
+        Node *newNode = new Node();
 };
 
