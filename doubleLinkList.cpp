905f49070c67209d7102d86a2abc79e5d5c9608f
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-05-15 11:04:23 +0700

    create new void hapus

--- doubleLinkList.cpp
+++ doubleLinkList.cpp
@@ -48,8 +48,27 @@ class DoubleLinkedList{
             newNode -> prev = current;
 
             if(current -> next != NULL){
-                
+                current -> next -> prev = newNode;
             }
+            current -> next = newNode;
+        }
+
+        void hapus(){
+            if (START == NULL){
+                cout << '\n List is empty'<< endl;
+                return;
+            }
+            cout << "\nEnter Number You Want To Delete: ";
+            int rollNo;
+            cin >> rollNo;
+
+            Node *current = START;
+
+            while (current != NULL && current -> noMhs != rollNo)
+            {
+                current = current -> next;
+            }
+            
         }
 };
 
